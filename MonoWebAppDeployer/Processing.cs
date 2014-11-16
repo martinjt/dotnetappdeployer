3431c4bb116f0354f83de9d6df096eadf226c1dd
Martin Thwaites <github@my2cents.co.uk>
2014-11-16 12:06:44 +0000

    Initial commit

---
+++ MonoWebAppDeployer/Processing.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+
+namespace MonoWebAppDeployer
+{
+	public class Processing
+	{
+		const string MSBUILD_NS = "http://schemas.microsoft.com/developer/msbuild/2003";
+		public static void ProcessFiles(FileInfo file, DirectoryInfo dir)
+		{
+			var doc = new XmlDocument();
+			doc.Load(file.FullName);
+
+			var ns = new XmlNamespaceManager(doc.NameTable);
+			ns.AddNamespace("msbld", MSBUILD_NS);
+
+			Console.WriteLine("Processing Content");
+			var nodes = doc.DocumentElement.SelectNodes("//msbld:Content", ns);
+			foreach (XmlElement element in nodes)
+			{
+				var source = Path.Combine(file.Directory.FullName, element.GetAttribute("Include").Replace("\\", "/"));
+				var destination = Path.Combine(dir.FullName, element.GetAttribute("Include").Replace("\\", "/"));
+				Directory.CreateDirectory(Path.GetDirectoryName(destination));
+				File.Copy(source, destination, true);
+				Console.WriteLine("Processing: {0}", source);
+			}
+			Console.WriteLine("Finished Processing Content");
+
+			var destBin = Directory.CreateDirectory(Path.Combine(dir.FullName, "bin"));
+			var binDir = new DirectoryInfo(Path.Combine(file.Directory.FullName, "bin"));
+
+			Console.WriteLine("Processing Bin files");
+			foreach (var binFile in binDir.GetFiles())
+			{
+				var source = binFile.FullName;
+				var destination = Path.Combine(destBin.FullName, binFile.Name);
+				File.Copy(source, destination, true);
+				Console.WriteLine("Processing: {0}", source);
+			}
+			Console.WriteLine("Finished Processing Bin Files");
+
+		}
+	}
+}
+
---
+++ MonoWebAppDeployer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace MonoWebAppDeployer
+{
+	class MainClass
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{			
+			var file = new FileInfo(args [0]);
+			if (Directory.Exists(args [1]))
+				Directory.Delete(args [1], true);
+
+			var dir = Directory.CreateDirectory(args [1]);
+
+			Processing.ProcessFiles(file, dir);
+			Console.ReadLine();
+		}
+	}
+}
